0f1808639fdd5032e20e546ad3715559d881e1d7
metaironia <serezha364@gmail.com>
2024-02-05 01:26:54 +0300

    added math expression computation in backend

---
+++ src/compiler_backend/backend.cpp
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include "../../lib/tree/tree_func.h"
+#include "../../lib/tree/tree_log.h"
+
+#include "../../lib/tree/math_tree/dsl_commands.h"
+#include "../../lib/tree/math_tree/math_tree_func.h"
+
+#include "backend.h"
+
+/*
+BackendFuncStatus TreeToAsmFile (FILE *asm_file, Tree *lang_tree, NameTable *name_table) {
+
+    assert (asm_file);
+    assert (lang_tree);
+    assert (name_table);
+
+    AsmFileBeginAndEnd (asm_file);
+
+    AsmFileNewFuncWrite (asm_file, lang_tree -> root, name_table);
+
+    return BACKEND_STATUS_OK;
+}
+
+BackendFuncStatus AsmFileBeginAndEnd (FILE *asm_file) {
+
+    assert (asm_file);
+
+    fprintf (asm_file, "call main\n"
+                       "hlt\n");
+
+    return BACKEND_STATUS_OK;
+}
+
+BackendFuncStatus AsmFileFuncNameWrite (FILE *asm_file, TreeNode *current_func_node, NameTable *name_table) {
+
+    assert (current_func_node);
+    assert (name_table);
+
+    TreeNode *func_name_node = current_func_node -> right_branch -> left_branch;
+
+    fprintf (asm_file, ":"); 
+    NodeTypePrint (asm_file, func_name_node, name_table);
+
+    return BACKEND_STATUS_OK;
+}
+
+BackendFuncStatus AsmFileNewFuncWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table) {
+
+    assert (current_node);
+    assert (name_table);
+
+    if (NODE_TYPE == LANGUAGE_OPERATOR) {
+
+        if (NODE_LANG_OPERATOR == NEW_FUNC) {
+
+            AsmFileFuncNameWrite     (asm_file, current_node -> right_branch, name_table);
+            AsmFileLangOperatorWrite (asm_file, 
+                                      CURRENT_FUNC_FIRST_END_LINE_NODE (current_node -> right_branch),
+                                      name_table);
+        }
+
+        else if (NODE_LANG_OPERATOR == INIT) {
+
+            AsmFileFuncNameWrite     (asm_file, current_node -> left_branch, name_table);
+            AsmFileLangOperatorWrite (asm_file, 
+                                      CURRENT_FUNC_FIRST_END_LINE_NODE (current_node -> left_branch),
+                                      name_table);
+
+            return BACKEND_STATUS_OK;
+        }
+    }
+
+    return AsmFileNewFuncWrite (asm_file, current_node -> left_branch, name_table);
+}
+
+BackendFuncStatus AsmFileLangOperatorWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table) {
+
+    assert (asm_file);
+    assert (name_table);
+
+    if (!current_node)
+        return BACKEND_STATUS_OK;
+
+    TreeNode *end_line_node = current_node;
+   
+    current_node = current_node -> left_branch;
+
+    if (NODE_TYPE == LANGUAGE_OPERATOR) 
+        switch (NODE_LANG_OPERATOR) {
+
+            case IF:
+                AsmFileOperatorIfWrite (asm_file, current_node, name_table);
+                break;
+
+            case WHILE:
+                AsmFileOperatorWhileWrite (asm_file, current_node, name_table);
+                break;
+
+            case ASSIGN:
+                AsmFileOperatorAssignWrite (asm_file, current_node, name_table);
+                break;
+        }
+
+    return AsmFileLangOperatorWrite (asm_file, end_line_node -> right_branch, name_table);    
+}
+
+BackendFuncStatus AsmFileOperatorIfWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table) {
+
+    assert (asm_file);
+    assert (current_node);
+    assert (name_table);
+
+//    AsmFileConditionWrite (asm_file, current_node -> left_branch, name_table);
+
+    AsmFileLangOperatorWrite (asm_file, current_node -> right_branch, name_table);
+
+    return BACKEND_STATUS_OK;
+}
+
+BackendFuncStatus AsmFileOperatorWhileWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table) {
+
+    assert (asm_file);
+    assert (current_node);
+    assert (name_table);
+
+//    AsmFileConditionWrite (asm_file, current_node -> left_branch, name_table);
+
+    AsmFileLangOperatorWrite (asm_file, current_node -> right_branch, name_table);
+
+    return BACKEND_STATUS_OK;
+}
+
+BackendFuncStatus AsmFileOperatorAssignWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table) {
+
+    assert (asm_file);
+    assert (current_node);
+    assert (name_table);
+
+
+
+    fprintf (asm_file, "pop []\n", );
+    return BACKEND_STATUS_OK;
+}
+*/
+BackendFuncStatus AsmFileMathExpressionWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table) {
+
+    assert (asm_file);
+    assert (current_node);
+    assert (name_table);  
+
+    if (NODE_TYPE == NUMBER || NODE_TYPE == VARIABLE) {
+
+        AsmFileVarOrNumWrite (asm_file, current_node, name_table);
+        return BACKEND_STATUS_OK;
+    }
+
+    if (AsmFileMathExpressionWrite (asm_file, current_node -> left_branch, name_table) == BACKEND_STATUS_FAIL)
+        return BACKEND_STATUS_FAIL;
+    
+    if (AsmFileMathExpressionWrite (asm_file, current_node -> right_branch, name_table) == BACKEND_STATUS_FAIL)
+        return BACKEND_STATUS_FAIL;
+
+    if (AsmFileMathOperatorWrite (asm_file, current_node) == BACKEND_STATUS_FAIL)
+        return BACKEND_STATUS_FAIL;
+
+    return BACKEND_STATUS_OK;
+}
+
+BackendFuncStatus AsmFileVarOrNumWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table) {
+
+    assert (asm_file);
+    assert (current_node);
+    assert (name_table);
+
+    if (NODE_TYPE == NUMBER)
+        fprintf (asm_file, "push %lf\n", NODE_VALUE);
+    
+    else if (NODE_TYPE == VARIABLE)
+        fprintf (asm_file, "push [%zu]\n", size_t (NODE_VALUE)); 
+
+    else 
+        return BACKEND_STATUS_FAIL;
+
+    return BACKEND_STATUS_OK;
+}
+
+BackendFuncStatus AsmFileMathOperatorWrite (FILE *asm_file, TreeNode *current_node) {
+
+    assert (asm_file);
+    assert (current_node);
+
+    if (NODE_TYPE == BINARY_OPERATOR) //TODO as function
+        switch (NODE_MATH_OPERATOR) {
+
+            case OPERATOR_ADD:
+                fprintf (asm_file, "add\n");
+                break;
+
+            case OPERATOR_SUB:
+                fprintf (asm_file, "sub\n");
+                break;
+
+            case OPERATOR_MUL:
+                fprintf (asm_file, "mul\n");
+                break;
+
+            case OPERATOR_DIV:
+                fprintf (asm_file, "div\n");
+                break;
+
+            default:
+                return BACKEND_STATUS_FAIL;
+        }
+
+    return BACKEND_STATUS_OK;
+}
---
+++ src/compiler_backend/backend.h
@@ -0,0 +1,41 @@
+#ifndef BACKEND_H
+#define BACKEND_H
+
+#include "../../lib/tree/tree_func.h"
+#include "../../lib/tree/tree_log.h"
+
+#include "../../lib/tree/math_tree/dsl_commands.h"
+#include "../../lib/tree/math_tree/math_tree_func.h"
+
+#define CURRENT_FUNC_FIRST_END_LINE_NODE(current_node)                                                      \
+                                                current_node -> right_branch -> left_branch -> right_branch
+
+enum BackendFuncStatus {
+
+    BACKEND_STATUS_FAIL,
+    BACKEND_STATUS_OK
+};
+
+BackendFuncStatus TreeToAsmFile (FILE *asm_file, Tree *lang_tree, NameTable *name_table);
+
+BackendFuncStatus AsmFileBeginAndEnd (FILE *asm_file);
+
+BackendFuncStatus AsmFileFuncNameWrite (FILE *asm_file, TreeNode *current_func_node, NameTable *name_table);
+
+BackendFuncStatus AsmFileNewFuncWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table);
+
+BackendFuncStatus AsmFileLangOperatorWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table);
+
+BackendFuncStatus AsmFileOperatorIfWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table);
+
+BackendFuncStatus AsmFileOperatorWhileWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table);
+
+BackendFuncStatus AsmFileOperatorAssignWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table);
+
+BackendFuncStatus AsmFileMathExpressionWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table);
+
+BackendFuncStatus AsmFileVarOrNumWrite (FILE *asm_file, TreeNode *current_node, NameTable *name_table);
+
+BackendFuncStatus AsmFileMathOperatorWrite (FILE *asm_file, TreeNode *current_node);
+
+#endif
